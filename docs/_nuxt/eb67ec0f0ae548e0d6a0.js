1077697efef6df9901a5b8a7bff9218cfc071cdc
Shunya Fujigaki <wisteria.shunya@gmail.com>
2020-07-05 17:12:05 +0900

    build

---
+++ docs/_nuxt/eb67ec0f0ae548e0d6a0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{424:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){window.location.href="https://std-ink.github.io/aliss/"}},l=e(38),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
