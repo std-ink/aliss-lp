45b01d252f92f718f916264225d7194f69a587e3
Shunya Fujigaki <wisteria.shunya@gmail.com>
2020-09-19 22:25:30 +0900

    build

--- docs/_nuxt/efb46673a2ab8767a095.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{439:function(t,n,e){"use strict";e.r(n);var o={mounted:function(){this.$store.dispatch("Logout")}},c=e(38),component=Object(c.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
